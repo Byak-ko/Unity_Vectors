c840c09ed36e79d20eec4ebc5f74645ab7adf4a2
Byak-ko <maxim993511@gmail.com>
2024-03-21 20:25:41 +0200

    Vectors

---
+++ Assets/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public GameObject Goal;
+    public float Speed;
+    private Vector3 direction;
+
+
+    void Start()
+    {
+        //_direction = Goal.transform.position - this.transform.position;
+
+    }
+
+    private void LateUpdate()
+    {
+        direction = Goal.transform.position - transform.position;
+        transform.LookAt(Goal.transform.position);
+        if (direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * Speed * Time.deltaTime;
+            transform.position = transform.position + velocity;
+        }
+
+
+    }
+}
